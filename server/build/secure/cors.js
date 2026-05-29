d9b05fd3b2f26f3fef353d6473431e681fdf9906
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2026-05-29 19:42:53 +0300

    init server cors config

--- server/build/secure/cors.js
+++ server/build/secure/cors.js
@@ -2,10 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const corsOptions = {
     origin: [
-        'https://alex-consultations.vercel.app',
+        'https://16-personalities-ang-express.vercel.app',
         'http://localhost:4200',
-        'https://vidchuttia.com.ua',
-        'http://vidchuttia.com.ua',
     ],
     methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
