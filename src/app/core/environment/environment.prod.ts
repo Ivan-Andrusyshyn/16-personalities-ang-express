9e7794358dffe25e76e48755114fd5762b9313db
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2026-05-29 19:32:58 +0300

    fix env

--- src/app/core/environment/environment.prod.ts
+++ src/app/core/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://16-personalities-ang-express-ylr4.vercel.app/',
+  apiUrl: 'https://16-personalities-ang-express-ylr4.vercel.app',
 };
